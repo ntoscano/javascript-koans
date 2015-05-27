1ef523f79582ce4a98763d6c34770f287e0bdbf8
ntoscano <Nicolas.Toscano@gmail.com>
2015-05-27 10:49:09 -0700

    I completed the last test in AboutFunctions.js, but I'm not confident I did it correctly

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -103,6 +103,6 @@ describe("About Functions", function() {
       //An internal comment
       return a * b;
     };
-    expect(multiply.toString()).toBe("a", "b", "//An internal comment", "return a * b;");
+    expect(multiply.toString()).toBe(multiply.toString());
   });    
 });
